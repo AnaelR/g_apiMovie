fa4f4c8523689fe09a712d183144e6fa90b9833f
AnaëlR <hey@anael-rodet.fr>
2022-01-21 15:53:19 +0100

    working routes

--- functions/routes/v1/categories.router.js
+++ functions/routes/v1/categories.router.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 const routes = (db) => {
+  //GET sur le path /categories
   router.get("/categories/", (req, res) => {
     db.collection("categories")
       .get()
@@ -11,7 +13,8 @@ const routes = (db) => {
           docs.push({ ...qds.data(), id: qds.id });
         });
         res.send(docs);
-      });
+      })
+      .catch(() => res.status(400).send("Error to find document"));
   });
 
   router.get("/categories/:id", (req, res) => {
@@ -20,11 +23,81 @@ const routes = (db) => {
       .get()
       .then((categorie) => {
         if (!categorie.exists) {
-          return res.send("No doc for '"+req.params.id+"' ID");
+          return res.send("No doc for '" + req.params.id + "' ID");
         } else {
           res.send({ ...categorie.data(), id: categorie.id });
         }
-      });
+      })
+      .catch(() => res.status(404).send("Error, be sure to add a correct ID"));
+  });
+
+  router.post(
+    "/categories/",
+
+    body("name").isString().notEmpty().exists(),
+
+    (req, res) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      args = {
+        name: req.body["name"],
+      };
+
+      db.collection("categories")
+        .add(args)
+        .then((doc) => res.status(201).send({ ...args, id: doc.id }))
+        .catch((error) => res.status(400).send(error));
+    }
+  );
+
+  router.put(
+    "/categories/:id",
+    body("name").isString().notEmpty(),
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const args = { name: req.body.name };
+
+      db.collection("categories")
+        .doc(req.params.id)
+        .update(args)
+        .then(() =>
+          db
+            .collection("categories")
+            .doc(req.params.id)
+            .get()
+            .then((doc) => res.status(202).send({ ...doc.data(), id: doc.id }))
+        )
+        .catch(() =>
+          res
+            .status(400)
+            .send("Error, be sure to add a correct ID and correct entry")
+        );
+    }
+  );
+
+  router.delete("/categories/:id", (req, res) => {
+    db.collection("categories")
+      .doc(req.params.id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          db.collection("categories")
+            .doc(req.params.id)
+            .delete()
+            .then(res.status(200).send("good delete"));
+        } else {
+          res.status(404).send("No categorie for this ID");
+        }
+      })
+      .catch(res.status(400));
   });
 
   return router;
--- functions/routes/v1/movies.router.js
+++ functions/routes/v1/movies.router.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var router = express.Router();
+const { body, validationResult } = require("express-validator");
+const { FieldValue } = require("firebase-admin").firestore;
 
 const routes = (db) => {
+  //GET sur le path /movies, affiche toutes les données de la collection "movies"
   router.get("/movies/", (req, res) => {
     db.collection("movies")
       .get()
@@ -11,9 +14,11 @@ const routes = (db) => {
           docs.push({ ...qds.data(), id: qds.id });
         });
         res.send(docs);
-      });
+      })
+      .catch(() => res.status(400).send("Error to find document"));
   });
 
+  //GET Affiche le film dont l'ID est passé après le path /movies/:id
   router.get("/movies/:id", (req, res) => {
     db.collection("movies")
       .doc(req.params.id)
@@ -24,22 +29,128 @@ const routes = (db) => {
         } else {
           res.send({ ...movie.data(), id: movie.id });
         }
-      });
+      })
+      .catch(() => res.status(400).send("Error, be sure to add a correct ID"));
   });
 
-  router.post("/movies/", (req, res) => {
-    if (
-      req.body.description &&
-      req.body.img &&
-      req.body.video &&
-      req.body.author &&
-      req.body.name
-    ) {
-      db.collection("movies").doc().set(req.body);
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(403);
+  //POST Poster un nouveau film, conditions etc.
+  router.post(
+    "/movies/",
+
+    body("description").isString().notEmpty().exists(),
+    body("img").isURL(),
+    body("video").isURL(),
+    body("author").isString().notEmpty().exists(),
+    body("name").isString().notEmpty().exists(),
+    body("categories_id").isString().notEmpty().exists(),
+
+    (req, res) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const img = req.body.img || "";
+      const video = req.body.video || "";
+
+      args = {
+        author: req.body["author"],
+        description: req.body["description"],
+        img: req.body["img"],
+        name: req.body["name"],
+        video: req.body["video"],
+        categories_id: req.body["categories_id"],
+      };
+
+      // const categorie = req.body.categorie_id || "";
+      let newObject = { ...args, likes: 0 };
+
+      db.collection("categories")
+        .doc(req.body.categories_id)
+        .get()
+        .then((doc) => {
+          if (!doc.exists) {
+            res.status(400).send("Bad categorie ID");
+          } else {
+            db.collection("movies")
+              .add(newObject)
+              .then((doc) => res.status(201).send({ ...newObject, id: doc.id }))
+              .catch((error) => res.status(400).send(error));
+          }
+        });
     }
+  );
+
+  //PATCH Update le film avec l'iD en option dans le path
+  router.patch(
+    "/movies/:id",
+
+    body("description").isString().optional(),
+    body("img").isURL().optional(),
+    body("video").isURL().optional(),
+    body("author").isString().optional(),
+    body("name").isString().optional(),
+    body("categories_id").isString().optional(),
+
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      args = {
+        ...req.body,
+      };
+
+      db.collection("movies")
+        .doc(req.params.id)
+        .update(args)
+        .then(() =>
+          db
+            .collection("movies")
+            .doc(req.params.id)
+            .get()
+            .then((doc) => res.status(201).send({ ...doc.data(), id: doc.id }))
+        )
+        .catch(() =>
+          res
+            .status(400)
+            .send("Error, be sure to add a correct ID and correct entry")
+        );
+    }
+  );
+
+  //PATCH Ajoute un like
+  router.patch("/movies/:id/like", (req, res) => {
+    db.collection("movies")
+      .doc(req.params.id)
+      .update({ likes: FieldValue.increment(1) })
+      .then((doc) =>
+        db
+          .collection("movies")
+          .doc(req.params.id)
+          .get()
+          .then((doc) => res.status(202).send(doc.data()))
+      )
+      .catch(() => res.status(400).send("Error, be sure to add a correct ID"));
+  });
+
+  //DELETE Movie
+  router.delete("/movies/:id", (req, res) => {
+    db.collection("movies")
+      .doc(req.params.id)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          db.collection("movies")
+            .doc(req.params.id)
+            .delete()
+            .then(res.status(200).send("good delete my friend"));
+        } else {
+          res.status(404).send("No movie for this ID");
+        }
+      });
   });
 
   return router;
